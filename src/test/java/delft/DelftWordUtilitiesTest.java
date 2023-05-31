2e5b2aaec9555351e038bc4b3b29fc5b5dd44918
davidcshepherd <davidshepherd@gmail.com>
2023-05-31 09:53:00 -0400

    Update DelftWordUtilitiesTest.java

--- src/test/java/delft/DelftWordUtilitiesTest.java
+++ src/test/java/delft/DelftWordUtilitiesTest.java
@@ -17,6 +17,7 @@ class DelftWordUtilitiesTest {
     @Test 
     public void testCamelCase(){
         assert(DelftWordUtilities.toCamelCase("bobby", false, null).equals("bobby"));
+        assert(DelftWordUtilities.toCamelCase("", false, null).equals(""));
         assert(DelftWordUtilities.toCamelCase("bobby", true, null).equals("Bobby"));
         assert(DelftWordUtilities.toCamelCase("bobbypsue", false, 'p').equals("bobbySue"));
         assert(DelftWordUtilities.toCamelCase("bobby;sue", false, ';').equals("bobbySue"));
