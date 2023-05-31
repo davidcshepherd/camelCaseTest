d592b6d3bfad94fe8bfed6478a59c0d838e5fe5f
davidcshepherd <davidshepherd@gmail.com>
2023-05-31 09:50:05 -0400

    update

--- src/test/java/delft/DelftWordUtilitiesTest.java
+++ src/test/java/delft/DelftWordUtilitiesTest.java
@@ -6,6 +6,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.beans.Transient;
 import java.util.*;
 import java.util.stream.*;
+
+import org.eclipse.osgi.internal.debug.Debug;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.*;
 import org.junit.jupiter.params.provider.*;
@@ -16,7 +18,8 @@ class DelftWordUtilitiesTest {
     public void testCamelCase(){
         assert(DelftWordUtilities.toCamelCase("bobby", false, null).equals("bobby"));
         assert(DelftWordUtilities.toCamelCase("bobby", true, null).equals("Bobby"));
-        assert(DelftWordUtilities.toCamelCase("bobbysue", false, 's').equals("bobbySue"));
+        assert(DelftWordUtilities.toCamelCase("bobbypsue", false, 'p').equals("bobbySue"));
+        assert(DelftWordUtilities.toCamelCase("bobby;sue", false, ';').equals("bobbySue"));
     }
 
 
