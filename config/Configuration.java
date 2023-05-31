828ce15021d371268dba27f18bd86b348602c8a3
davidcshepherd <davidshepherd@gmail.com>
2023-05-31 08:37:13 -0400

    Update Configuration.java

--- config/Configuration.java
+++ config/Configuration.java
@@ -24,91 +24,10 @@ public class Configuration extends RunConfiguration {
         return List.of("delft.DelftWordUtilities");
     }
 
-    @Override
-    public List<MetaTest> metaTests() {
-        return List.of(
-                MetaTest.insertAt("the string is empty", 28,
-                        """
-                         if ("".equals(str)) {
-                            throw new RuntimeException("killed the mutant");
-                         }
-                         """),
-                MetaTest.insertAt("the string is null", 28,
-                        """
-                         if (str == null) {
-                            throw new RuntimeException("killed the mutant");
-                         }
-                         """),
-                MetaTest.insertAt("the string is one lowercase letter", 32,
-                        """
-                        if (str.matches("[a-z]")) {
-                            throw new RuntimeException("killed the mutant");
-                        }
-                        """),
-                MetaTest.insertAt("the string is one uppercase letter", 32,
-                        """
-                        if (str.matches("[A-Z]")) {
-                            throw new RuntimeException("killed the mutant");
-                        }
-                        """),
-                MetaTest.insertAt("the string is one character whitespace", 32,
-                        """
-                        if (str.matches(" ")) {
-                            throw new RuntimeException("killed the mutant");
-                        }
-                        """),
-                MetaTest.insertAt("the string is multiple lowercase characters", 32,
-                        """
-                        if (str.matches("[a-z]+")) {
-                            throw new RuntimeException("killed the mutant");
-                        }
-                        """),
-                MetaTest.insertAt("the string is multiple uppercase characters", 32,
-                        """
-                        if (str.matches("[A-Z]+")) {
-                            throw new RuntimeException("killed the mutant");
-                        }
-                        """),
-                MetaTest.insertAt("the string has more than 3 characters", 33,
-                        """
-                        if (buffer.length > 3) {
-                            throw new RuntimeException("killed the mutant");
-                        }
-                        """),
-                MetaTest.withStringReplacement("negates first if-statement",
-                        "Character.isUpperCase(ch)",
-                        "!Character.isUpperCase(ch)"),
-                MetaTest.withStringReplacement("negates second if-statement",
-                        "Character.isLowerCase(ch)",
-                        "!Character.isLowerCase(ch)"),
-                MetaTest.withStringReplacement("does only work for upper case characters",
-                        """
-                        else if (Character.isLowerCase(ch)) {
-                            buffer[i] = Character.toUpperCase(ch);
-                        }
-                        """,
-                        ""),
-                MetaTest.withStringReplacement("does not work for upper case characters",
-                        """
-                        if (Character.isUpperCase(ch)) {
-                            buffer[i] = Character.toLowerCase(ch);
-                        """,
-                        """
-                        if (Character.isUpperCase(ch)) {
-                            buffer[i] = Character.toUpperCase(ch);
-                        """),
-                MetaTest.withStringReplacement("does not work for lower case characters",
-                        """
-                        else if (Character.isLowerCase(ch)) {
-                            buffer[i] = Character.toUpperCase(ch);
-                        }
-                        """,
-                        """
-                        else if (Character.isLowerCase(ch)) {
-                            buffer[i] = Character.toLowerCase(ch);
-                        }
-                        """)
-        );
-    }
+
+    public int numberOfMutationsToConsider() {
+        return 15;
+    }    
+    
 
 }
