821350d95bc5d0562ef933f76288726a29866336
davidcshepherd <davidshepherd@gmail.com>
2023-05-31 08:33:03 -0400

    Update DelftWordUtilitiesTest.java

--- src/test/java/delft/DelftWordUtilitiesTest.java
+++ src/test/java/delft/DelftWordUtilitiesTest.java
@@ -3,6 +3,7 @@ package delft;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.beans.Transient;
 import java.util.*;
 import java.util.stream.*;
 import org.junit.jupiter.api.*;
@@ -10,4 +11,12 @@ import org.junit.jupiter.params.*;
 import org.junit.jupiter.params.provider.*;
 
 class DelftWordUtilitiesTest {
+
+    @Test 
+    public void testCamelCase{
+        assert(DelftWordUtilities.toCamelCase("bobby", false, null).equals("bobby"));
+        assert(DelftWordUtilities.toCamelCase("bobby", true, null).equals("Bobby"));
+    }
+
+
 }
