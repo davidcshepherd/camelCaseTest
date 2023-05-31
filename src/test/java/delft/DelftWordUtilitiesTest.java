d628b68131127dc446e19f16a4d45f19a6407c5c
davidcshepherd <davidshepherd@gmail.com>
2023-05-31 10:36:53 -0400

    Update DelftWordUtilitiesTest.java

--- src/test/java/delft/DelftWordUtilitiesTest.java
+++ src/test/java/delft/DelftWordUtilitiesTest.java
@@ -2,21 +2,26 @@ package delft;
 
 import org.junit.jupiter.api.*;
 
+import static org.assertj.core.api.Assertions.*;
+
+
 class DelftWordUtilitiesTest {
 
     @Test 
     public void testCamelCase(){
-        assert(DelftWordUtilities.toCamelCase("bobby", false, null).equals("bobby"));
-        assert(DelftWordUtilities.toCamelCase("BObby", false, null).equals("bobby"));
-        assert(DelftWordUtilities.toCamelCase("", false, null).equals(""));
+        assertThat(DelftWordUtilities.toCamelCase("bobby", false, null).equals("bobby"));
+        assertThat(DelftWordUtilities.toCamelCase("BObby", false, null).equals("bobby"));
+        assertThat(DelftWordUtilities.toCamelCase("", false, null).equals(""));
         DelftWordUtilities.toCamelCase(null, false, null);
-        assert(DelftWordUtilities.toCamelCase("bobby", true, null).equals("Bobby"));
-        assert(DelftWordUtilities.toCamelCase("bobbypsue", false, 'p').equals("bobbySue"));
-        assert(DelftWordUtilities.toCamelCase("bobby;sue", false, ';').equals("bobbySue"));
-        assert(DelftWordUtilities.toCamelCase(";bobby;sue", true, ';').equals("BobbySue"));
-        assert(DelftWordUtilities.toCamelCase(";bobby;sue", false, ';').equals("bobbySue"));
-        assert(DelftWordUtilities.toCamelCase("bobby;;sue", false, ';').equals("bobbySue"));
-        assert(DelftWordUtilities.toCamelCase("bobby;;;;;;;;sue", false, ';').equals("bobbySue"));
+        assertThat(DelftWordUtilities.toCamelCase("bobby", true, null).equals("Bobby"));
+        assertThat(DelftWordUtilities.toCamelCase("bobbypsue", false, 'p').equals("bobbySue"));
+        assertThat(DelftWordUtilities.toCamelCase("bobby;sue", false, ';').equals("bobbySue"));
+        assertThat(DelftWordUtilities.toCamelCase(";bobby;sue", true, ';').equals("BobbySue"));
+        assertThat(DelftWordUtilities.toCamelCase(";bobby;sue", false, ';').equals("bobbySue"));
+        assertThat(DelftWordUtilities.toCamelCase("bobby;;sue", false, ';').equals("bobbySue"));
+        assertThat(DelftWordUtilities.toCamelCase("bobby;;;;;;;;sue", false, ';').equals("bobbySue"));
+        assertThat(DelftWordUtilities.toCamelCase("bobbyXXsue", false, 'X').equals("bobbyxxsue"));
+        assertThat(DelftWordUtilities.toCamelCase("bobbyxsueysue", false, 'x','y').equals("bobbySueSue"));
     }
 
 
